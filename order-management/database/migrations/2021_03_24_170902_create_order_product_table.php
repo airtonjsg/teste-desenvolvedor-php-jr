87137369d9a17dc3c1025cdf9824de6cd1279e1e
airton.garcia <airtonjsg@gmail.com>
2021-03-24 19:12:32 -0300

    feat: <relação N X N - pedido X produto>.
    <Changes to be committed:
    modified:   database/migrations/2021_03_24_170902_create_order_product_table.php
    >.
    
    * Implementação(ões):
    1 -
    2 -
    3 -
    * ...
    
    refs #..... @... #..... @...

--- order-management/database/migrations/2021_03_24_170902_create_order_product_table.php
+++ order-management/database/migrations/2021_03_24_170902_create_order_product_table.php
@@ -18,8 +18,8 @@ class CreateOrderProductTable extends Migration
             $table->unsignedInteger('order_id');
             $table->foreign('order_id')->references('id')->on('order');
 
-            $table->unsignedInteger('client_id');
-            $table->foreign('client_id')->references('id')->on('clients');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
 
         });
     }
